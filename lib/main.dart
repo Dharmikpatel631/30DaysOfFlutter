cea187aba9418c31f7ddcb2544d62c055b1f83fe
dharmikpate <pateldharmik631@gmail.com>
2022-03-29 04:48:23 -0700

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catk/home_page.dart';
+import 'package:flutter_catk/pages/home_page.dart';
+import 'package:flutter_catk/pages/login_page.dart';
+
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => loginpage(),
+        "/home": (context) => Homepage(),
+        "/login":(context) => loginpage(),
+      },
     );
   }
 }
