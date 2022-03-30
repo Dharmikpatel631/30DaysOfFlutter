8f7224394aa9d956c6a7eeaa93e3dfb681e345e8
dharmikpate <pateldharmik631@gmail.com>
2022-03-30 07:23:53 -0700

    day6 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catk/pages/home_page.dart';
 import 'package:flutter_catk/pages/login_page.dart';
+import 'package:flutter_catk/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -18,13 +19,14 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.lato().fontFamily,
       ),
+      debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       routes: {
         "/": (context) => loginpage(),
-        "/home": (context) => Homepage(),
-        "/login": (context) => loginpage(),
+        Myroutes.homeRoutes: (context) => Homepage(),
+        Myroutes.loginRoutes: (context) => loginpage(),
       },
     );
   }
